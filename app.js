fe9f833b82604040f9510f053e477bf838232698
dndkwk <rmswn3147@gmail.com>
2020-11-29 19:15:32 +0900

    첫번째 시작페이지 추가했습니다. total로 매출량 총량 구하는것만 하면됩니다.

--- app.js
+++ app.js
@@ -13,85 +13,77 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
-//root페이지
-app.get('/15', function (req, res) {
-    res.render('15.ejs');
+var total=0;
+
+//매니저 메뉴 페이지
+app.get('/manager_menu', function (req, res) {
+    res.render('manager_menu.ejs');
     //res.send('ROOT');
 });
 
-/*
-//list 페이지
-app.get('/list', function (req, res) {
-    var sql = 'SELECT * FROM BOARD';    
-    conn.query(sql, function (err, rows, fields) {
-        if(err) console.log('query is not excuted. select fail...\n' + err);
-        else res.render('list.ejs', {list : rows});
-    });
+//시작 페이지
+app.get('/first', function (req, res) {
+    res.render('first.ejs');
 });
 
-app.get('/write', function (req, res) {
-    res.render('write.ejs');
-});
-
-app.post('/writeAf', function (req, res) {
-    var body = req.body;
-    console.log(body);
-
-    var sql = 'INSERT INTO BOARD VALUES(?, ?, ?, NOW())';
-    var params = [body.id, body.title, body.content];
-    console.log(sql);
-    conn.query(sql, params, function(err) {
-        if(err) console.log('query is not excuted. insert fail...\n' + err);
-        else res.redirect('/list');
-    });
-});
-*/
-
-//메뉴편집 페이지
-app.get('/16',function(req,res){
+//메뉴편집 페이지 edit_menu
+app.get('/edit_menu',function(req,res){
     var sql = 'SELECT * FROM ITEM';    
     conn.query(sql, function (err, rows, fields) {
         if(err) console.log('query is not excuted. select fail...\n' + err);
-        else res.render('16.ejs', {ITEM : rows});
+        else res.render('edit_menu.ejs', {ITEM : rows});
     });
-    
 });
 
-//리뷰확인 페이지
-app.get('/17',function(req,res){
+//리뷰확인 페이지 review_confirm
+app.get('/review_confirm',function(req,res){
     var sql = 'SELECT * FROM REVIEW';    
     conn.query(sql, function (err, rows, fields) {
         if(err) console.log('query is not excuted. select fail...\n' + err);
-        else res.render('17.ejs', {review : rows});
+        else res.render('review_confirm.ejs', {review : rows});
     });
 });
 
-//데이터 페이지
-app.get('/18',function(req,res){
+//매출량 페이지 sale
+app.get('/sale',function(req,res){
+    var sql = 'SELECT * FROM FINANCE';    
+    conn.query(sql, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        else res.render('sale.ejs', {FINANCE : rows, sell_total : total});
+    });
+});
+
+//선호도 페이지 preference
+/*
+app.get('/19',function(req,res){
     var sql = 'SELECT * FROM BOARD';    
     conn.query(sql, function (err, rows, fields) {
         if(err) console.log('query is not excuted. select fail...\n' + err);
-        else res.render('18.ejs', {data : rows});
+        else res.render('19.ejs', {data : rows});
     });
-});
+});*/
 
 //삭제 기능
 app.get('/delete/:menu', function (req, res) {
     conn.query('delete from ITEM where name=?', [req.params.menu], function () {
-        res.redirect('/16')
+        res.redirect('/edit_menu')
       });
 });
 
 //추가 기능
-app.post('/16', function (req, res) {
+app.post('/edit_menu', function (req, res) {
     const body = req.body
-    conn.query('insert into ITEM (name, price,ItemNum) values (?, ?, ?);', [
+    conn.query('insert into ITEM (name, price) values (?, ?);', [
       body.menu,
-      body.price,
-      body.ItemNum
+      body.price
     ], function() {
-      res.redirect('/16')
+      res.redirect('/edit_menu')
     });
   });
+
+
+
+//이미지 경로
+app.use(express.static('./static'));
 
 app.listen(3000, () => console.log('Server is running on port 3000...'));
